da389e01744ac292d284888323e156a9c80e9a15
ZeLars3 <zelt378@gmail.com>
2022-06-12 23:48:47 +0300

    feature/install firebase

--- apps/client/src/environments/environment.ts
+++ apps/client/src/environments/environment.ts
@@ -4,6 +4,15 @@
 
 export const environment = {
   production: false,
+  firebase: {
+    apiKey: "AIzaSyB27PSQ0plUvqzxU99ewgSI18a-o8hylpY",
+    authDomain: "angular-kanban-6d56b.firebaseapp.com",
+    projectId: "angular-kanban-6d56b",
+    storageBucket: "angular-kanban-6d56b.appspot.com",
+    messagingSenderId: "41259934586",
+    appId: "1:41259934586:web:2ea7df6a88b62630c92cc3",
+    measurementId: "G-754NTQJNH3"
+  }
 };
 
 /*
